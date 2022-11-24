5cd04c0888b6a361a1958bcd531f0a4a45b50bc6
Sehan MacPro <ianparkfirst@gmail.com>
2022-11-24 17:31:17 +0900

    fix: env

--- config/application.rb
+++ config/application.rb
@@ -19,7 +19,14 @@ module DimplServer
     end
     config.before_configuration do
       env_file = File.join(Rails.root, 'config', '.env')
-      Dotenv.overload(env_file) if File.exists?(env_file)
+      if File.exists?(env_file)
+        if Rails.env.development?
+          Dotenv.overload(env_file) 
+        else
+          Dotenv.load(env_file) 
+        end
+      end
+      pp ENV
     end
     config.generators do |g|
       g.orm :active_record, primary_key_type: :uuid
