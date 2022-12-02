212aaeff7f116d41b62dcc0f71485f6cd7d1b0a7
Sehan MacPro <ianparkfirst@gmail.com>
2022-12-02 11:21:07 +0900

    feat: update aws

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,6 +1,6 @@
 CDN_HOST = ENV['STORAGE_CDN_HOST']
 Aws.config.update({
-  region: ENV['STORAGE_AWS_REGION'],
+  region: ENV['AWS_REGION'],
   credentials: Aws::Credentials.new(ENV['STORAGE_ACCESS_KEY_ID'],  ENV['STORAGE_SECRET_ACCESS_KEY']),
 })
 private_key_file = Rails.root.join("config", "keys", "private_key.pem")
